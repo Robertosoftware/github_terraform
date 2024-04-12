db9b49327cd2492e6032d8e94c392d8924015bf2
Roberto Bonilla Ibarra <robertodgb@gmail.com>
2024-04-12 13:47:02 +0200

    refreshed backend

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,12 +4,6 @@ provider "azurerm" {
 
 
 terraform {
-  backend "azurerm" {
-    resource_group_name  = var.state_resource_group_name
-    storage_account_name = var.state_storage_account_name
-    container_name       = "tfstate"
-    key                  = "terraform-base.tfstate"
-  }
   required_version = ">= 1.1.0"
   required_providers {
     azurerm = {
