e2785be65bd85a0346e4825a87c6489898c4739c
Roberto Bonilla Ibarra <robertodgb@gmail.com>
2024-04-12 13:34:16 +0200

    Terraform Cloud

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,3 +16,22 @@ resource "azurerm_storage_container" "main" {
   storage_account_name  = azurerm_storage_account.main.name
   container_access_type = "private"
 }
+
+resource "azurerm_resource_group" "demo" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+##  Demo now
+resource "azurerm_storage_account" "StorageAccountDemo" {
+  name                     = "satestant000012"
+  resource_group_name      = azurerm_resource_group.demo.name
+  location                 = azurerm_resource_group.demo.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    video = "azure"
+    channel = "CloudQuickLabs"
+  }
+}
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,6 +2,7 @@ provider "azurerm" {
   features {}
 }
 
+
 terraform {
   backend "azurerm" {
     resource_group_name  = var.state_resource_group_name
@@ -9,6 +10,20 @@ terraform {
     container_name       = "tfstate"
     key                  = "terraform-base.tfstate"
   }
+  required_version = ">= 1.1.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+  cloud {
+    organization = "inteliia"
+    workspaces {
+      name = "engineering-workspace"
+    }
+  }
 }
+
 
 data "azurerm_client_config" "current" {}
