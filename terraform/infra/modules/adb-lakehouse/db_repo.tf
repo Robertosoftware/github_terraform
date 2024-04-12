04cb4f7502f71252aa8a01977982d3fd4d8353aa
Roberto Bonilla Ibarra <robertodgb@gmail.com>
2024-04-12 22:24:14 +0200

    Repos Quality Control

--- terraform/infra/modules/adb-lakehouse/db_repo.tf
+++ terraform/infra/modules/adb-lakehouse/db_repo.tf
@@ -1,12 +1,12 @@
 # Mounting databricks repo
 resource "databricks_repo" "dbrepo01" {
   url = "https://github.com/Robertosoftware/databricks-demo-engineering.git"
-  path = "/Repos/code-repo/"
+  path = "/Repos/Engineering/code-repo/"
   git_provider = "gitHub"
 }
 
 resource "databricks_repo" "dbrepo02" {
   url = "https://github.com/Robertosoftware/Databricks-First-Class-MLOps.git"
-  path = "/Repos/mlops/"
+  path = "/Repos/AI/mlops/"
   git_provider = "gitHub"
 }
